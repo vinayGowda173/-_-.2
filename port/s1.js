8b01c371736537b960b9aa4e03c7ca4a756a8a7e
vinayGowda173 <vinaygowd150@gmail.com>
2025-03-12 19:34:57 +0530

    Add files via upload

---
+++ port/s1.js
@@ -0,0 +1,17 @@
+console.log(document.querySelector(".contents"));
+function resume()
+{
+    window.open("resume.html","self");
+}
+function certificates()
+{
+    window.open("certificates.html","self");
+}
+function academic()
+{
+    window.open("academic.html","self");
+}
+function projects()
+{
+    window.open("projects.html","self");
+}
